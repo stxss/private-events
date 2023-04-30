5d0079d31172c0f8271d932913ee05ba5c5aad2e
stxss <93729036+stxss@users.noreply.github.com>
2023-04-30 22:40:28 +0100

    Setup authentication and adapt login
    Allow user to sign in using email or username

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,31 @@
 class EventsController < ApplicationController
+    before_action :require_login, only: [:new, :create]
+
     def index
         @events = Event.all
+    end
+
+    def new
+        @event = Event.new
+    end
+
+    def create
+        @event = current_user.events.build(event_params)
+
+        if @event.save
+            redirect_to root_path
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def require_login
+        user_signed_in?
+    end
+
+    def event_params
+        params.require(:event).permit(:title, :body)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :events
   root "events#index"
+
+  devise_for :users
+  resources :events
 end
