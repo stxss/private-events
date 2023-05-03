cff1dbd5e3fea66cfd69ce83947c199c3a1f32ac
stxss <93729036+stxss@users.noreply.github.com>
2023-05-03 17:28:26 +0100

    Create past and upcoming events display on Event index page

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,12 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :event_attendances, foreign_key: :attended_event_id
   has_many :attendees, through: :event_attendances, source: :attendee
+
+  def self.past
+    Event.where("start_date < ? ", Date.current)
+  end
+
+  def self.upcoming
+    Event.where("start_date > ? ", Date.current)
+  end
 end
