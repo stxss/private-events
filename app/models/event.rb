356bc8622e8d2790926e7586d0254f526e2d4633
stxss <93729036+stxss@users.noreply.github.com>
2023-05-06 22:47:13 +0100

    Add some validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,10 @@
 class Event < ApplicationRecord
+  validate :ok_date?, on: %i[create new edit update]
+
+  validates :title, presence: true, length: { in: 6..20 }
+  validates :description, presence: true, length: { in: 20..500 }
+
+
   belongs_to :creator, class_name: "User"
   has_many :event_attendances, foreign_key: :attended_event_id
   has_many :attendees, through: :event_attendances, source: :attendee, dependent: :destroy
@@ -18,6 +24,21 @@ class Event < ApplicationRecord
     end
   end
 
+  private
+
+  def ok_date?
+    if start_date < Time.current
+      errors.add(:start_date, "can't be in the past")
+    end
+
+    if end_date < start_date
+      errors.add(:start_date, "can't end before start")
+    elsif end_date == start_date
+      if end_time < start_time
+        errors.add(:start_date, "can't end before start")
+      end
+    end
+  end
   # def self.past
   # Event.where("start_date < ? ", Date.current)
   # end
