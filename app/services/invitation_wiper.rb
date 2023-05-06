b061d9922a52574fa8af05bc8d0e337a416de619
stxss <93729036+stxss@users.noreply.github.com>
2023-05-06 20:50:14 +0100

    Create invitations destroyer

---
+++ app/services/invitation_wiper.rb
@@ -0,0 +1,13 @@
+class InvitationWiper < ApplicationService
+    def initialize(params)
+      @host = params[:inviter_id]
+      @event = params[:event_id]
+      @invitees = params[:invitee_ids].reject(&:blank?)
+    end
+
+    def call
+      @invitees.each do |person|
+        invitation = Invitation.where(event_id: @event, inviter_id: @host, invitee_id: person).delete_all
+      end
+    end
+  end
